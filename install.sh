1d70b85bec31bd60fdb775d9b1b7680f43bb1e39
grezar <grezar.dev@gmail.com>
2018-07-29 09:56:52 +0900

    Installing dein.vim

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@
 set -u
 
 DOTPATH=$(cd $(dirname $0) && pwd)
+DEINPATH="$DOTPATH"/.vim/dein/repos/github.com/Shougo/dein.vim
 
+# Creating symbolic links
 for dotfile in .??*; do
   [ "$dotfile" = ".git" ] && continue
   [ "$dotfile" = ".gitignore" ] && continue
 
   ln -nsfv "$DOTPATH"/"$dotfile" "$HOME"/"$dotfile"
 done
+
+# Installing dein.vim
+[ ! -d "$DEINPATH" ] && git clone https://github.com/Shougo/dein.vim.git "$DEINPATH"
